e25728e168c764eb7cdad486df8bd931f74ab258
Ali-Huseyn-525 <133035543+Ali-Huseyn-525@users.noreply.github.com>
2023-07-16 14:59:58 +0400

    Create 2606.cpp

---
+++ Week 8/2606.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+int main(){
+	long long a,b;
+	cin>>a>>b;
+	if(a>=b){
+		cout<<b<<" "<<a;
+	}
+	else
+	cout<<a<<" "<<b;
+    return 0;
+
+}
