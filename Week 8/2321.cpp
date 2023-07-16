940a207fa8370c23775c3364c32715e3d40e03ca
Ali-Huseyn-525 <133035543+Ali-Huseyn-525@users.noreply.github.com>
2023-07-16 15:12:59 +0400

    Create 2321.cpp

---
+++ Week 8/2321.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<algorithm>
+using namespace std;
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int a[n];
+    for(int i=0;i<n;i++)scanf("%d",&a[i]);
+    sort(a,a+n);
+    for(int i=0;i<n;i++)printf("%d ",a[i]);
+}
