f1e3752aa3694e838453f5a3134a048b6a15ab2e
Kenan0610 <139623379+Kenan0610@users.noreply.github.com>
2023-07-23 15:03:37 +0400

    Create 1.cpp

---
+++ Week 1/1.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main ()
+{
+//long long  abcd,a,b,c,d;
+//	
+//	scanf("%lld",&abcd);
+//	a=abcd/1000;
+//	b=abcd/100%10;;
+//	c=abcd/10%10;
+//	d=abcd%10;
+//	printf("%lld",b*10+c);
+//	return 0;
+//	
+long long ab,a,b;
+scanf("%d",&ab);
+a=ab/10;
+b=ab%10;
+printf("%d %d",a,b);
+}
